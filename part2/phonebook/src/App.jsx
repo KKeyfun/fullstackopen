f2f9947fe514f0bc8fd735f1cfb1bb6a97489a52
kkeyfun <KKeyfun@users.noreply.github.com>
2023-04-27 21:36:40 -0700

    Added phone number field

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,41 +2,53 @@ import { useState } from 'react';
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas', number: '123-456-7890' },
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  function checkIssues(name) {
+  function checkIssues(name, number) {
     for (const person of persons) {
       if (person.name === name) {
-        return 'nameConflict';
+        return true;
       }
-      if (name === '') {
-        return 'empty';
+      if (person.number === number) {
+        return true;
       }
     }
     return false;
   }
 
-  function newPerson(event) {
+  function updateNameInput(event) {
     setNewName(event.target.value);
   }
 
   function addPerson(event) {
     event.preventDefault();
-    if (checkIssues(newName) === 'nameConflict') {
-      alert(`${newName} is already in phonebook`);
-    } else if (checkIssues(newName) === 'empty') {
-      alert('Name field is empty');
+    if (checkIssues(newName, newNumber)) {
+      alert(`${newName} or ${newNumber} is already in phonebook`);
+    } else if (newName === '' || newNumber === '') {
+      alert('Name/Number field is empty');
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
       setNewName('');
+      setNewNumber('');
     }
+  }
+
+  function updateNumberInput(event) {
+    setNewNumber(event.target.value);
   }
 
   function Display({ list }) {
     return (
-      list.map((person) => <li key={person.name}>{person.name}</li>)
+      list.map((person) => (
+        <li key={person.name}>
+          {person.name}
+          {' '}
+          {person.number}
+        </li>
+      ))
     );
   }
 
@@ -47,7 +59,12 @@ function App() {
         <div>
           name:
           {' '}
-          <input onChange={newPerson} />
+          <input onChange={updateNameInput} value={newName} />
+        </div>
+        <div>
+          number:
+          {' '}
+          <input value={newNumber} onChange={updateNumberInput} />
         </div>
         <div>
           <button type="submit" onClick={addPerson}>add</button>
