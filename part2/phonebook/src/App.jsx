d5a05e1514dd69d56685c42106ce8d1ec6b9646c
kkeyfun <KKeyfun@users.noreply.github.com>
2023-04-28 16:32:17 -0700

    Moved persons data to db.json to be used with json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function Header({ text }) {
   return (
@@ -31,18 +33,23 @@ function PersonForm({ inputs, button }) {
 }
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [filteredPersons, setFiltered] = useState([...persons]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log(response);
+        setPersons(response.data);
+        setFiltered(response.data);
+      });
+  }, []);
 
   function checkIssues(name, number) {
     for (const person of persons) {
